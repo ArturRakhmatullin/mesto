5371dbea288d3f67db8858a476c6aafc9c4a9d62
Рахматуллин Артур Ранильевич <a.rahmatullin@agroexport.local>
2022-02-12 00:10:24 +0300

    hotfix/iteration1

--- scripts/script.js
+++ scripts/script.js
@@ -4,21 +4,20 @@ const profileProfession = root.querySelector('.profile__profession');
 let popup = root.querySelector('.popup');
 let pencil = root.querySelector('.profile__rename');
 let close = root.querySelector('.popup__close');
-let formElement = root.querySelector('.popup__container');
-let nameInput = root.querySelector('.popup__nameforedit');
-let jobInput = root.querySelector('.popup__professionforedit');
-
+let formElement = root.querySelector('.popup__form');
+let nameInput = root.querySelector('.popup__placefortext_name');
+let jobInput = root.querySelector('.popup__placefortext_profession');
 
 function openPopup() {
-    popup.classList.add('popup-opened');
+    popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileProfession.textContent;
+    console.log('Hello');
 }
 
 function closePopup() {
-    popup.classList.remove('popup-opened');
+    popup.classList.remove('popup_opened');
 }
-
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
