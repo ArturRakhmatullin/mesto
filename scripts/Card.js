fb4683726facb1f8ace163d930e7b83d4af27d0e
Рахматуллин Артур Ранильевич <a.rahmatullin@agroexport.local>
2022-07-20 16:07:41 +0300

    PR7/start1

---
+++ scripts/Card.js
@@ -0,0 +1,94 @@
+import { openPopup } from "./index.js";
+
+export default class Card {
+	  constructor(data, cardSelector) {
+	    this._title = data.name;
+	    this._imageLink = data.link;
+	    this._cardSelector = cardSelector;
+	    this._element = this._getTemplate();
+	  }
+	
+	  _getTemplate() {
+	    const cardElement = document
+	      .querySelector(this._cardSelector)
+	      .content.querySelector(".elements__cardname")
+	      .cloneNode(true);
+	
+	    return cardElement;
+	  }
+
+	  _like(evt) {
+	    evt.target.classList.toggle("elements__like_active");
+	  }
+
+	  _removeCard(evt) {
+	    evt.target.closest(".elements__cardname").remove();
+	  }
+
+	  _scaleImage(evt) {
+	    this._popupForFullscreen = document.querySelector('.popup_type_fullscreen');
+	    this._formImg = document.querySelector('.popup__image');
+	    this._formImgTitle = document.querySelector('.popup__name');
+	    this._formImg.src = evt.target.src;
+	    this._formImg.alt = evt.target.alt;
+	    this._formImgTitle.textContent = evt.target.alt;
+	
+	    return openPopup(this._popupForFullscreen);
+	  }
+	
+	  _setEventListener() {
+	    this._image = this._element.querySelector(".elements__photo");
+	    this._element
+	      .querySelector(".elements__like")
+	      .addEventListener("click", this._like);
+	    this._element
+	      .querySelector(".elements__delete")
+	      .addEventListener("click", this._removeCard);
+	    this._image.addEventListener("click", this._scaleImage);
+	  }
+	
+	  generateCard() {
+	    this._setEventListener();
+	
+	    this._image.src = this._imageLink;
+	    this._image.alt = this._title;
+	    this._element.querySelector(".elements__cardname").textContent = this._title;
+
+	    return this._element;
+	  }
+	}
+
+  const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  function renderCards() {
+    initialCards.forEach((el) => {
+      cards.append(createCard(el));
+    });
+ }
+ 
+ renderCards();
---
+++ scripts/index.js
@@ -0,0 +1,123 @@
+import Card from "./Card.js";
+import FormValidator from "./FormValidator.js";
+
+const selectorsNamesForValidation = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__info",
+  submitButtonSelector: ".popup__submit",
+  inactiveButtonClass: "popup__submit_inactive",
+  inputErrorClass: "form__info_type_error",
+  errorClass: "popup__error",
+};
+
+const popupList = document.querySelectorAll(".popup");
+const popupForEditAuthor = document.querySelector(".popup_type_rename");
+const popupForAddCard = document.querySelector(".popup_type_append");
+const profileEditOpenBtn = document.querySelector(".profile__rename");
+const cardAddOpenBtn = document.querySelector(".profile__button");
+
+const formForEditAuthor = popupForEditAuthor.querySelector(".popup__form_rename");
+const formForAddCard = popupForAddCard.querySelector(".popup__form_type_append-card");
+const formInputImage = formForAddCard.querySelector(".popup__info_type_email");
+const formInputTitle = formForAddCard.querySelector(".popup__info_type_place");
+
+const authorProfile = document.querySelector(".profile__name");
+const authorJobProfile = document.querySelector(".profile__profession");
+const authorProfileInput = document.querySelector(".popup__info_type_name");
+const authorJobProfileInput = document.querySelector(".popup__info_type_profession");
+
+const cardElements = document.querySelector(".cards");
+
+const validatorFormForEditAuthor = new FormValidator(
+  selectorsNamesForValidation,
+  formForEditAuthor
+);
+const validatorFromForAddCard = new FormValidator(
+  selectorsNamesForValidation,
+  formForAddCard
+);
+
+export function openPopup(popup) {
+  popup.classList.add("popup_opened");
+  document.addEventListener("keydown", closeByEsc);
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+  document.removeEventListener("keydown", closeByEsc);
+}
+
+function closeByEsc(evt) {
+  if (evt.key === "Escape") {
+    const popupVisible = document.querySelector(".popup_opened");
+    closePopup(popupVisible);
+  }
+}
+
+function openPropfilePopup() {
+  authorProfileInput.value = authorProfile.textContent;
+  authorJobProfileInput.value = authorJobProfile.textContent;
+  validatorFormForEditAuthor.resetValidation();
+  openPopup(popupForEditAuthor);
+}
+
+function submitProfileInfo(evt) {
+  evt.preventDefault();
+  authorProfile.textContent = authorProfileInput.value;
+  authorJobProfile.textContent = authorJobProfileInput.value;
+  closePopup(popupForEditAuthor);
+}
+
+function renderCard(elementPlace, element) {
+  elementPlace.prepend(element);
+}
+
+function handleNewCard(card) {
+  const newCard = new Card(card, "#card").generateCard();
+  return newCard;
+}
+
+function submitAddCard(evt) {
+  evt.preventDefault();
+
+  const cardContainer = [];
+  cardContainer.link = formInputImage.value;
+  cardContainer.name = formInputTitle.value;
+
+  renderCard(cardElements, handleNewCard(cardContainer));
+
+  closePopup(popupForAddCard);
+  formForAddCard.reset();
+}
+
+window.onload = function () {
+  const body = document.querySelector('.root');
+  body.style.display = 'flex';
+};
+
+validatorFormForEditAuthor.enableValidation();
+validatorFromForAddCard.enableValidation();
+
+profileEditOpenBtn.addEventListener("click", openPropfilePopup);
+cardAddOpenBtn.addEventListener("click", () => {
+  formForAddCard.reset();
+  validatorFromForAddCard.resetValidation();
+  openPopup(popupForAddCard);
+});
+
+formForEditAuthor.addEventListener("submit", submitProfileInfo);
+formForAddCard.addEventListener("submit", submitAddCard);
+
+popupList.forEach((item) => {
+  item.addEventListener("mousedown", function (evt) {
+    if (
+      evt.target.classList.contains("popup_opened") || evt.target.classList.contains("popup__close")
+    ) {
+      closePopup(item);
+    }
+  });
+});
+
+
+
+export { openPopup }
